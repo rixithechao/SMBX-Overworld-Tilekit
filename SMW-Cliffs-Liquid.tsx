992062fef98b521c1c40bfff04affceae3d6ab31
rockythechao <mack151.rockythechao@yahoo.com>
2020-03-05 19:35:28 -0600

    corrected a typo, added missing shore tiles, exported a tileset that was seemingly embedded for the sake of consistency with the others
    gound

--- SMW-Cliffs-Liquid.tsx
+++ SMW-Cliffs-Liquid.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="SMW - Cliffs/Liquid" tilewidth="32" tileheight="32" tilecount="120">
+<tileset version="1.2" tiledversion="1.3.3" name="SMW - Cliffs/Liquid" tilewidth="32" tileheight="32" tilecount="120" columns="15">
  <image source="smwCliffsAndLiquid.png" width="480" height="256"/>
 </tileset>
